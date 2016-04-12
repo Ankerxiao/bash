3794f4a7ca3cea6a567039cb389467233bca5da9
xiao <807132689@qq.com>
2016-04-12 13:28:39 +0800

    第二次更改

--- mysql.sh
+++ mysql.sh
@@ -13,31 +13,30 @@ case $1 in
         sql="show tables from $2"
         ;;
     create_table)
-        #sql="use $2"
-        sql="create table Class.MyClass(
+        sql="create table $2.$3(
              id int(4) not null primary key auto_increment,
              name char(20) not null,
              sex int(4) not null default '0',
-             degree double(16,2))"
+             degree double(16,2),
+             address char(100) not null)"
         ;;
     des_table)
-        sql="use $2"
-        sql="desc $3"
+        sql="desc $2.$3"
         ;;
     select)
-        sql="select * from test.user"
+        sql="select * from $2.$3"
         ;;
     delete)
-        sql="delete from test.user where id=$2"
+        sql="delete from $2.$3 where id=$4"
         ;;
     insert)
-        sql="insert into test.user(username,password) values('$2','$3')"
+        sql="insert into $2.$3(username,password) values('$4','$5')"
         ;;
     update)
-        sql="update test.user set username='$3',password='$4' where id='$2'"
+        sql="update $2.$3 set username='$3',password='$4' where id='$4'"
         ;;
     delete_table)
-        sql="drop table $2"
+        sql="drop table $2.$3"
         ;;
     delete_database)
         sql="drop database $2"
