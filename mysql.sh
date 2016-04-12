0f93a6e3d87c466bb71f878bac287d60998287b4
xiao <807132689@qq.com>
2016-04-12 13:04:54 +0800

    one

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,27 @@
 conn="mysql -uroot -proot"
 
 case $1 in
+    show_databases)
+        sql="show databases"
+        ;;
+    create_database)
+        sql="create database $2"
+        ;;
+    show_tables)
+        sql="show tables from $2"
+        ;;
+    create_table)
+        #sql="use $2"
+        sql="create table Class.MyClass(
+             id int(4) not null primary key auto_increment,
+             name char(20) not null,
+             sex int(4) not null default '0',
+             degree double(16,2))"
+        ;;
+    des_table)
+        sql="use $2"
+        sql="desc $3"
+        ;;
     select)
         sql="select * from test.user"
         ;;
@@ -14,6 +35,12 @@ case $1 in
         ;;
     update)
         sql="update test.user set username='$3',password='$4' where id='$2'"
+        ;;
+    delete_table)
+        sql="drop table $2"
+        ;;
+    delete_database)
+        sql="drop database $2"
         ;;
 esac
 $conn -e "$sql"
