2cfcd210189aa9efe5a45e366329b941787dd5ba
xiao <807132689@qq.com>
2016-04-13 14:44:55 +0800

    隐藏输入的密码

--- login.sh
+++ login.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 read -p "请输入用户名: " user
-read -p "请输入密码: " password
+read -p "请输入密码: " -s password
+
 
 if [ $user != "root" ]
 then
-    echo "用户名错误"
+    echo "\n用户名错误"
 elif [ $password != "123456" ]
 then
-    echo "密码错误"
+    echo -e "\n密码错误"
 else
-    echo "成功"
+    echo -e "\n成功"
 fi
  
